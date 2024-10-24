b0e1fe896672e9f4ab17bee45a612cb019240b62
Sierra555 <dara6234@gmail.com>
2024-10-24 12:49:10 +0300

    added search

--- src/components/SearchInput/SearchInput.js
+++ src/components/SearchInput/SearchInput.js
@@ -4,21 +4,23 @@ import { Search } from 'react-feather';
 
 function SearchInput({ placeholder, query, setQuery }) {
   return (
-    <div className='input-wrapper' tabIndex={1}>
-      <label htmlFor="search-input">
-        <VisuallyHidden>{placeholder}</VisuallyHidden>
-      </label>
-      <Search aria-hidden="true" />
-      <input
-        type="search"
-        className='input'
-        id="search-input"
-        name="search-form"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        placeholder={placeholder}
-      />
-    </div>
+    <search>
+      <div className='input-wrapper' tabIndex={1}>
+        <label htmlFor="search-input">
+          <VisuallyHidden>{placeholder}</VisuallyHidden>
+        </label>
+        <Search aria-hidden="true" />
+        <input
+          type="search"
+          className='input'
+          id="search-input"
+          name="search-form"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder={placeholder}
+        />
+      </div>
+    </search>
   );
 }
 
